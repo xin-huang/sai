04f2761bdc5144fc9532be84c3c51adf485b76ed
xin-huang <xinhuang.res@gmail.com>
2024-11-12 13:46:58 +0100

    Update

--- sai/parsers/score_parser.py
+++ sai/parsers/score_parser.py
@@ -92,7 +92,7 @@ def _run_score(args: argparse.Namespace) -> None:
         win_len=args.win_len,
         win_step=args.win_step,
         num_src=args.num_src,
-        anc_allele_file=args.anc_allele_file,
+        anc_allele_file=args.anc_alleles,
         ploidy=args.ploidy,
         is_phased=args.is_phased,
         w=args.w,
@@ -170,11 +170,11 @@ def add_score_parser(subparsers: argparse.ArgumentParser) -> None:
         help="number of source populations to include. Default is 1.",
     )
     parser.add_argument(
-        "--anc-allele-file",
-        dest="anc_allele_file",
+        "--anc-alleles",
+        dest="anc_alleles",
         type=existed_file,
         default=None,
-        help="path to the file with ancestral allele information. Default is None.",
+        help="path to the BED file with ancestral allele information. Default is None.",
     )
     parser.add_argument(
         "--ploidy",
